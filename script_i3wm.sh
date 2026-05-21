3bc1c46acffaa66832db3ef49be992a46801d9bd
ClisneyMarcelo <marceloclisney@gmail.com>
2026-05-21 15:41:22 -0400

    Create script_i3wm.sh
    Instalação do i3wm

---
+++ script_i3wm.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Script para executab pelo terminal
+$ sudo apt install i3-wm xorg py3status suckless-tools rofi fzf wmctrl pcmanfm lxappearance nitrogen fonts-open-sans fonts-inconsolata arc-theme xz-utils bash-completion alsa-utils pipewire pipewire-audio vlc htop mousepad
