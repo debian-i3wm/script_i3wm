ff34a72f9558e5cffb21e847a45f35bde4428a62
debian-i3wm <marceloclisney@gmail.com>
2026-05-22 09:53:11 -0400

    script_i3wm.sh
    Instalação do desktop i3wm e aplivativos

--- script_i3wm.sh
+++ script_i3wm.sh
@@ -1,3 +1,39 @@
-#!/bin/bash
-# Script para executar no terminal
-sudo apt install i3-wm xorg py3status suckless-tools rofi fzf wmctrl pcmanfm lxappearance nitrogen fonts-open-sans fonts-inconsolata arc-theme xz-utils bash-completion alsa-utils pipewire pipewire-audio vlc htop mousepad
+#!/bin/bash 
+comandos=( 
+
+    "echo 'Iniciar a instalação do desktop... (atualizando pacotes)' && sudo apt install i3-wm xorg py3status suckless-tools xfce4-terminal rofi fzf wmctrl pcmanfm lxappearance nitrogen fonts-open-sans fonts-inconsolata arc-theme xz-utils bash-completion alsa-utils pipewire pipewire-audio" 
+    "echo 'Instalar aplicativo básicos... (Instalar aplicativos)' && sudo apt install qimgv evince vlc htop mousepad flameshot" 
+    "echo 'Instalar pacote libreoffice... (Instalar aplicativo libreoffice)' && sudo apt install --no-install-recommends libreoffice-writer libreoffice-calc libreoffice-impress" 
+)  
+echo "=== Automação de Comandos ===" 
+echo 
+for comando in "${comandos[@]}"; do 
+    echo "----------------------------------------" 
+    echo "Comando pendente: $comando" 
+    echo "----------------------------------------" 
+    while true; do 
+        read -p "Digite (e) para iniciar, (p) pular ou (s) sair do script?:" opcao 
+        opcao=$(echo "$opcao" | tr '[:upper:]' '[:lower:]') 
+        case "$opcao" in 
+            e) 
+                echo "-> Executando..." 
+                eval "$comando" 
+                echo "-> Comando finalizado." 
+                break # Sai deste while e vai para o próximo comando 
+                ;; 
+            p) 
+                echo "-> Comando pulado." 
+                echo 
+                break # Sai deste while e vai para o próximo comando 
+                ;; 
+            s) 
+                echo "-> Saindo do script..." 
+                exit 0 
+                ;; 
+            *) 
+                echo "Opção inválida! Digite 'e' para iniciar, 'p' para pular ou 's' para sair." 
+                ;; 
+        esac 
+    done 
+done 
+echo "Concluido com sucesso" 
