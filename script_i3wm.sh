ef91e12efe2dbf0cd7d41c86324dc5c1a129586d
debian-i3wm <marceloclisney@gmail.com>
2026-05-22 16:04:40 -0400

    script_i3wm.sh
    Instalação do desktop i3wm

--- script_i3wm.sh
+++ script_i3wm.sh
@@ -4,12 +4,15 @@ comandos=(
     "echo 'Iniciar a instalação do desktop... (atualizando pacotes)' && sudo apt install i3-wm xorg py3status suckless-tools xfce4-terminal rofi fzf wmctrl pcmanfm lxappearance nitrogen fonts-open-sans fonts-inconsolata arc-theme xz-utils bash-completion alsa-utils pipewire pipewire-audio" 
     "echo 'Instalar aplicativo básicos... (Instalar aplicativos)' && sudo apt install qimgv evince vlc htop mousepad flameshot" 
     "echo 'Instalar pacote libreoffice... (Instalar aplicativo libreoffice)' && sudo apt install --no-install-recommends libreoffice-writer libreoffice-calc libreoffice-impress" 
+    "echo 'Instalar login... (Instalar login)' && sudo apt install lightdm lightdm-gtk-greeter" 
+    "echo 'Ativar login... (Ativar login)' && sudo systemctl enable lightdm" 
+    "echo 'Iniciar login... (Iniciar login)' && sudo systemctl start lightdm" 
 )  
 echo "=== Automação de Comandos ===" 
 echo 
 for comando in "${comandos[@]}"; do 
     echo "----------------------------------------" 
-    echo "Comando pendente: $comando" 
+    echo "Informe a letra para continuar: $comando" 
     echo "----------------------------------------" 
     while true; do 
         read -p "Digite (e) para iniciar, (p) pular ou (s) sair do script?:" opcao 
