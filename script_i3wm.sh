21ed2d9baa1dbdd5045bd737a61a8a41762cea09
debian-i3wm <marceloclisney@gmail.com>
2026-05-22 16:21:29 -0400

    script_i3wm.sh
    Instalação do Dektop i3wm

--- script_i3wm.sh
+++ script_i3wm.sh
@@ -1,33 +1,32 @@
 #!/bin/bash 
 comandos=( 
-
-    "echo 'Iniciar a instalação do desktop... (atualizando pacotes)' && sudo apt install i3-wm xorg py3status suckless-tools xfce4-terminal rofi fzf wmctrl pcmanfm lxappearance nitrogen fonts-open-sans fonts-inconsolata arc-theme xz-utils bash-completion alsa-utils pipewire pipewire-audio" 
+    "echo 'Iniciar a instalação do desktop... (Instalar desktop)' && sudo apt install i3-wm xorg py3status suckless-tools xfce4-terminal rofi fzf wmctrl pcmanfm lxappearance nitrogen fonts-open-sans fonts-inconsolata arc-theme xz-utils bash-completion alsa-utils pipewire pipewire-audio" 
     "echo 'Instalar aplicativo básicos... (Instalar aplicativos)' && sudo apt install qimgv evince vlc htop mousepad flameshot" 
     "echo 'Instalar pacote libreoffice... (Instalar aplicativo libreoffice)' && sudo apt install --no-install-recommends libreoffice-writer libreoffice-calc libreoffice-impress" 
     "echo 'Instalar login... (Instalar login)' && sudo apt install lightdm lightdm-gtk-greeter" 
     "echo 'Ativar login... (Ativar login)' && sudo systemctl enable lightdm" 
     "echo 'Iniciar login... (Iniciar login)' && sudo systemctl start lightdm" 
 )  
-echo "=== Automação de Comandos ===" 
+echo "=== INSTALAÇÃO DO DESKTOP i3wm ===" 
 echo 
 for comando in "${comandos[@]}"; do 
     echo "----------------------------------------" 
-    echo "Informe a letra para continuar: $comando" 
+    echo "INFORME A LETRA PARA CONTINUAR: $comando" 
     echo "----------------------------------------" 
     while true; do 
-        read -p "Digite (e) para iniciar, (p) pular ou (s) sair do script?:" opcao 
+        read -p "Digite (e) para iniciar, (p) pular ou (s) sair?:" opcao 
         opcao=$(echo "$opcao" | tr '[:upper:]' '[:lower:]') 
         case "$opcao" in 
             e) 
                 echo "-> Executando..." 
                 eval "$comando" 
                 echo "-> Comando finalizado." 
-                break # Sai deste while e vai para o próximo comando 
+                break
                 ;; 
             p) 
                 echo "-> Comando pulado." 
                 echo 
-                break # Sai deste while e vai para o próximo comando 
+                break
                 ;; 
             s) 
                 echo "-> Saindo do script..." 
